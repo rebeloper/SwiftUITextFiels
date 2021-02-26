5f23c7f832d01c6c6de17f2c3cc1c3f274fb1275
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-26 18:03:26 +0200

    Initial Commit

---
+++ Shared/SwiftUITextFielsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextFielsApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 26.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextFielsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
